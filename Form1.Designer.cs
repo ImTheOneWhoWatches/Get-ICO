e3c124483778798914e95f6604c55b74bb456d79
TheOneWhoWatches <162903308+ImTheOneWhoWatches@users.noreply.github.com>
2024-11-10 22:34:27 -0500

    Added Source Code

---
+++ Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace GrabICO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.processStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // processStart
+            // 
+            this.processStart.BackColor = System.Drawing.Color.AliceBlue;
+            this.processStart.Location = new System.Drawing.Point(60, 46);
+            this.processStart.Name = "processStart";
+            this.processStart.Size = new System.Drawing.Size(162, 174);
+            this.processStart.TabIndex = 0;
+            this.processStart.Text = "Start Process";
+            this.processStart.UseVisualStyleBackColor = false;
+            this.processStart.Click += new System.EventHandler(this.processStart_Click);
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.OutlineButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(27)))), ((int)(((byte)(67)))));
+            this.ClientSize = new System.Drawing.Size(285, 273);
+            this.Controls.Add(this.processStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Watches ~ ICO GRABBER";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button processStart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GrabICO
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void processStart_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Executable Files (*.exe)|*.exe";
+            if (!string.IsNullOrEmpty(fileDialog.Filter) && fileDialog.ShowDialog() == DialogResult.OK)
+                try
+                {
+                    Icon icon = Icon.ExtractAssociatedIcon(fileDialog.FileName);
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Filter = "Icon Files (*.ico)|*.ico";
+
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                        using (FileStream stream = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                        {
+                            icon.Save(stream);
+                        }
+                }
+
+                catch (Exception)
+                {
+                    
+                }
+
+        }
+    }
+}
